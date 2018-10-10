6cb61a012891eefdf82916572567b39f936178c9
Spectateur_ <37877869+emile971@users.noreply.github.com>
2018-10-10 18:09:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
+var Discord = require("discord.js");
+bot = new Discord.Client();
+client = new Discord.Client();
+var prefix = ("a!");
+bot.on('ready', () => {
+    console.log("Correctement connecter!");
+    avatar = "https://cdn.discordapp.com/attachments/498529437512368135/498914619793080356/Screenshot_20181008-194458.png"
+    bot.user.setAvatar(avatar);
+    bot.user.setUsername('! Tôhru',"")
+});
+ 
+bot.on("ready", () => {
+ 
+  bot.user.setGame(`Bot en DEV | Tohru Army`);
+});
 
+bot.on('message', message =>{
+  var splited_message = message.content.slice(prefix.length).split(" ");
+  var parameters = splited_message.slice(1)
+  
+  if(message.content === "a!say")
+  message.delete();
+  var toRepeat = parameters.join(" ");
+                if (toRepeat === "") {
+                    return message.channel.send("? **Utilisation:** "+prefix+"say <message>")
+                };
+                toRepeat = toRepeat .replace("@everyone", "@??everyone")
+                  .replace("@here", "@??here");
+ 
+                  var embed = new Discord.RichEmbed()
+ 
+                  .setAuthor(bot.user.username, bot.user.avatarURL)    
+                  .setDescription(toRepeat)
+                  .setColor("RANDOM");
+     message.channel.send(embed);
+    
+ 
+     
+
+});
+
+bot.login(process.env.TOKEN)
